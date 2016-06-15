e2f18ae70b6138570de03b137e1291a06a3886fe
Sebastian Mischke <SebastianMischke@gmx.de>
2016-06-15 14:29:09 +0200

    Hinzufügen von JUnit-Tests

---
+++ test/testPerson.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import se.*;
+
+public class testPerson {
+	@Test
+	public void testKonstruktor() {
+		Person p1 = new Person(null, null);
+		Assert.assertEquals(1, 1);
+	}
+
+	// TODO: Tests hinzufügen
+}
